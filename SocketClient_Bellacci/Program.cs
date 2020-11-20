abeb875c6e62c3c207f1f2a937a0f9ef564e56b0
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-20 12:01:14 +0100

    connessione a server

--- SocketClient_Bellacci/Program.cs
+++ SocketClient_Bellacci/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
 
 namespace SocketClient_Bellacci
 {
@@ -6,7 +8,55 @@ namespace SocketClient_Bellacci
     {
         static void Main(string[] args)
         {
-            
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            //ha bisogno di un endpoint dove connettersi
+            string strIPAddress = "";
+            string strPort = "";
+            IPAddress ipAddress = null;
+            int nPort;
+
+            //il client è a più rischio di errore: cade connessione, morte server, lettura porta errata
+            try
+            {
+                Console.WriteLine("IP del server: ");
+                strIPAddress = Console.ReadLine();
+
+                Console.WriteLine("Porta del server: ");
+                strPort = Console.ReadLine();
+
+                //prova a inserire la stringa che scrivo in un tipo ipaddress, se non ci riesce non da errore
+                //se ci riesce da true
+                //il punto esclamativo inverse le cose, quindi se NON ci riesce da true
+                if (!IPAddress.TryParse(strIPAddress.Trim(), out ipAddress))
+                {
+                    Console.WriteLine("IP non valido.");
+                    return;
+                }
+
+                if (!int.TryParse(strPort, out nPort))
+                {
+                    Console.WriteLine("Porta non valida.");
+                    return;
+                }
+
+                if (nPort <= 0 || nPort >= 65535) 
+                {
+                    Console.WriteLine("Porta non valida.");
+                    return;
+                }
+
+                Console.WriteLine("EndPoint Server: " + ipAddress.ToString() + " " + nPort);
+
+                //effettiva connessione al serve
+                //sblocca la asset nel server
+                //per collegarsi al socket server 127.0.0.1  23000
+                client.Connect(ipAddress, nPort);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
